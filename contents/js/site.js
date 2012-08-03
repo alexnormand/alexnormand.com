a54962aac7707d69e0d19587a98a65920418f71d
Alex Normand <anormnd@gmail.com>
2012-08-04 01:30:22 +0200

    cleanup site.js file

--- contents/js/site.js
+++ contents/js/site.js
@@ -1,70 +1,71 @@
 define(['jquery', 'hljs'], function($, hljs) {
-	var site  = {
+    var site  = {
 
-	    updateMainContent: function(link) {
-		      $.get(link.href, function(data){
+        updateMainContent: function(link) {
+            $.get(link.href, function(data){
 
-		          if (window.location.host === 'www.alexnormand.com') {
-			            _gaq.push(['_trackPageview', link.href]);
-		          }
+                if (window.location.host === 'www.alexnormand.com') {
+                    _gaq.push(['_trackPageview', link.href]);
+                }
 
-		          $('section.content').replaceWith($(data).find('.content'));
-		          $('section.content').children()
-                  .hide()
-                  .fadeIn(500);
+                $('section.content').replaceWith($(data).find('.content'));
+                $('section.content').children()
+                    .hide()
+                    .fadeIn(500);
 
-		          $(window).scrollTop(0); //for mobile browsers
-		          $('title').text($(data).filter('title').text()); //set new page title
+                $(window).scrollTop(0); //for mobile browsers
+                $('title').text($(data).filter('title').text()); //set new page title
 
-              //highligh code blocks
-              $('pre code').each(function(i, e) {
-                  if ($(e).attr('class') === undefined) {
-                      $(e).attr('class', 'no-highlight');
-                  }
-                  hljs.highlightBlock(e);
-              });
-		      });
-	    },
-      
-	    init: function() {
+                //highligh code blocks
+                $('pre code').each(function(i, e) {
+                    if ($(e).attr('class') === undefined) {
+                        $(e).attr('class', 'no-highlight');
+                    }
+                    hljs.highlightBlock(e);
+                });
+            });
+        },
 
-		      if(!!(window.history && history.pushState)) {
-		          $('#mainmain').on('click', 'a', function(e){
-			            var link = e.target;
-			            if(link.host === window.location.host) {
-			                site.updateMainContent(link);
-			                history.pushState(null, null, link.href);
-			                e.preventDefault();
-			            }
-		          });
-              
-		          window.setTimeout(function() {
-			            $(window).bind('popstate', function(e) {
-			                site.updateMainContent(window.location);
-			            });
-		          }, 1000);
-		      }
-          
+        init: function() {
 
-		      $('select.main_nav').change(function() {
-		          var option = $(this).find(':selected');
-              
-		          option.prop('selected', true);
-              
-		          if (option.val().indexOf('/') === 0) {
-			            site.updateMainContent({href: option.val()});
-		          } else {
-			            window.location = option.val();
-		          }
-              
-		      });
-          
-          //init highlight.js
-          hljs.initHighlighting();
-    
-	    }
-	};
-    
+            if (!!(window.history && history.pushState)) {
+                $('#mainmain').on('click', 'a', function(e){
+                    var link = e.target;
+                    if (link.host === window.location.host) {
+                        site.updateMainContent(link);
+                        history.pushState(null, null, link.href);
+                        e.preventDefault();
+                    }
+                });
+
+                window.setTimeout(function() {
+                    $(window).bind('popstate', function(e) {
+                        site.updateMainContent(window.location);
+                    });
+                }, 1000);
+            }
+
+
+            $('select.main_nav').change(function() {
+                var option = $(this).find(':selected');
+
+                option.prop('selected', true);
+
+                if (option.val().indexOf('/') === 0) {
+                    site.updateMainContent({href: option.val()});
+                } else {
+                    window.location = option.val();
+                }
+
+            });
+
+            //init highlight.js
+            hljs.initHighlighting();
+
+        }
+    };
+
     return site;
 });
+
 
