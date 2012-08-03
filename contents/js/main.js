580cf78efd10cfcdaaf6089757dcf39c34635bfc
Alex Normand <anormnd@gmail.com>
2012-08-04 01:32:11 +0200

    removed order and text require.js plugins

--- contents/js/main.js
+++ contents/js/main.js
@@ -1,14 +1,12 @@
 require.config({
     paths: {
- 	order: 'libs/require/order',
 	jquery: 'libs/jquery',
         hljs:   'libs/highlight.pack',
 	site:   'site'
     }
 });
 
-
-require(['order!jquery', 'order!hljs', 'order!site'], function($, hljs, site) {
+require(['jquery', 'hljs', 'site'], function($, hljs, site) {
 
         $(function() {
 	    site.init();
