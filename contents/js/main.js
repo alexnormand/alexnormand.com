e3178fe283ea090ba238f547932749b973611bfd
Alex Normand <anormnd@gmail.com>
2012-11-13 00:57:20 +0100

    js loading

--- contents/js/main.js
+++ contents/js/main.js
@@ -9,7 +9,18 @@ require.config({
 require(['jquery', 'prettify', 'site'], function($, prettify, site) {
 
   $(function() {
+    window._gaq = window._gaq || [];
+    _gaq.push(['_setAccount', 'UA-27119715-3']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
     site.init();
+
 	});
 });
 
