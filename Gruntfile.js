563da4c8cedbe62b6b46c798adf870481b634df2
Alex Normand <anormnd@gmail.com>
2014-03-02 02:29:18 +0100

    replaced social icon fonts by inline svg images

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,13 +39,6 @@ module.exports = function (grunt) {
           },
           {
             expand: true,
-            cwd:  wintersmithBuildDir +'/font',
-            src: ['**'],
-            dest: appengineStaticdir +'/font'
-          },
-
-          {
-            expand: true,
             cwd:  wintersmithBuildDir,
             src: [
               'index.html',
